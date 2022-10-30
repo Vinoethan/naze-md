5e62c0f463a7a01fe0f363e047583244e1706a72
Vinoethan <116991130+Vinoethan@users.noreply.github.com>
2022-10-30 11:04:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -31,11 +31,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['6289501248500']
+global.ownernomer = "6289501248500"
+global.premium = ['6289501248500']
 global.packname = 'Sticker By'
-global.author = 'Naze Dev'
+global.author = 'Vino Bot'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -66,9 +66,9 @@ global.mygit = 'https://github.com/nazedev'
 global.myyt = 'https://youtube.com/c/Nazedev'
 global.myytv = 'https://youtu.be/ey7qLuRBm5Y'
 global.mygc = "https://chat.whatsapp.com/Khpnk6svwIhCIvx91Xi0Vf"
-global.botname = 'Naze Dev'
+global.botname = 'Jem Bot'
 global.akulaku = 'Bot By Naze'
-global.ytname = 'YT NAZE'
+global.ytname = 'No YT'
 
 
 let file = require.resolve(__filename)
